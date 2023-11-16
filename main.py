9948a097860620f2ab551a1accabfec4ecbb9077
Jacoroch <110855576+Jacoroch@users.noreply.github.com>
2023-11-16 16:31:03 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from model import RappitenderoModel
+from view import RappitenderoView
+from controller import RappitenderoController
+
+def main():
+    root = tk.Tk()
+    model = RappitenderoModel('localhost', 'root', 'CONTRASENA.', 'rappi2')  # Asegúrate de usar la contraseña correcta aquí
+    view = RappitenderoView(root, None)  # Inicialmente pasamos None para el controlador
+    controller = RappitenderoController(view, model)
+    view.controller = controller  # Ahora establecemos el controlador en la vista
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
