48517119bb649c207edfb74a9904ea0f180da2b7
Jacoroch <110855576+Jacoroch@users.noreply.github.com>
2023-11-16 17:48:45 -0500

    Update view.py

--- view.py
+++ view.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import simpledialog
+
+
 
 class RappitenderoView:
     def __init__(self, master, controller):
@@ -53,6 +56,13 @@ class RappitenderoView:
         # Widget para mostrar todos los rappitenderos
         self.button_mostrar_rappitenderos = tk.Button(master, text="Mostrar todos los Rappitenderos", command=self._on_mostrar_rappitenderos)
         self.button_mostrar_rappitenderos.pack()
+        
+        #eliminar
+        self.button_eliminar_rappitendero = tk.Button(master, text="Eliminar Rappitendero", command=self._on_eliminar_rappitendero)
+        self.button_eliminar_rappitendero.pack()
+        #actualizar
+        self.update_button = tk.Button(master, text="Actualizar Rappitendero", command=self.update_rappitendero)
+        self.update_button.pack()
 
     def _on_create(self):
         # Este método se activa cuando se presiona el botón Crear Rappitendero
@@ -63,6 +73,26 @@ class RappitenderoView:
         calificacion = self.entry_calificacion.get()
         telefono = self.entry_telefono.get()
         self.controller.create_rappitendero(nombre, afiliado_salud, dias_trabajados, horas_trabajadas, calificacion, telefono)
+        
+    def _on_eliminar_rappitendero(self):
+        try:
+            # Solicitar al usuario el ID del Rappitendero a eliminar
+            id_rappitendero = self.pedir_id()
+
+            if id_rappitendero is not None:
+                # Llama al método del controlador para eliminar el Rappitendero
+                self.controller.delete_rappitendero(id_rappitendero)
+                self.show_message("Rappitendero eliminado exitosamente")
+        except Exception as e:
+            self.show_message("Error al eliminar Rappitendero: " + str(e))
+    
+    
+    def pedir_id(self):
+        # Utiliza un cuadro de diálogo de entrada de datos para que el usuario ingrese el ID
+        id_rappitendero = simpledialog.askinteger("Eliminar Rappitendero", "Ingrese el ID del Rappitendero a eliminar:")
+        return id_rappitendero
+
+    
     def _on_mostrar_rappitenderos(self):
         try:
             # Llama al método del controlador para obtener la lista de rappitenderos
@@ -80,6 +110,20 @@ class RappitenderoView:
         # Este método se activa cuando se presiona el botón Buscar
         nombre_busqueda = self.entry_buscar.get()
         self.controller.buscar_rappitendero(nombre_busqueda)
+        
+    def update_rappitendero(self):
+        id_to_update = simpledialog.askstring("Actualizar Rappitendero", "Ingrese el ID del Rappitendero a actualizar")
+        if id_to_update:
+            # Solicitar los nuevos valores para los campos
+            nombre = simpledialog.askstring("Actualizar Rappitendero", "Nuevo nombre:")
+            afiliado_salud = simpledialog.askstring("Actualizar Rappitendero", "Nuevo poliza de salud:")
+            dias_trabajados = simpledialog.askinteger("Actualizar Rappitendero", "Nuevos días trabajados:")
+            horas_trabajadas = simpledialog.askinteger("Actualizar Rappitendero", "Nuevas horas trabajadas:")
+            calificacion = simpledialog.askfloat("Actualizar Rappitendero", "Nueva calificación:")
+            telefono = simpledialog.askstring("Actualizar Rappitendero", "Nuevo teléfono:")
+
+            # Llamar al controlador para actualizar
+            self.controller.update_rappitendero(id_to_update, nombre, afiliado_salud, dias_trabajados, horas_trabajadas, calificacion, telefono)
 
     def show_message(self, message):
         messagebox.showinfo("Información", message)
